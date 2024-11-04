87941cb76f77c9af2ea8776324f71e13acd9bc07
simeon.gor <simeon.gor@yandex.ru>
2024-11-04 18:11:46 +0300

    feat: change history order

--- src/main/java/com/simeon/lab2/beans/History.java
+++ src/main/java/com/simeon/lab2/beans/History.java
@@ -5,7 +5,7 @@ import jakarta.inject.Named;
 
 import java.io.Serial;
 import java.io.Serializable;
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Objects;
 
@@ -16,14 +16,14 @@ public class History implements Serializable {
     @Serial
     private static final long serialVersionUID = 4078483457730741479L;
 
-    private final List<CheckResult> resultList = new ArrayList<>();
+    private final List<CheckResult> resultList = new LinkedList<>();
 
     public List<CheckResult> getResultList() {
         return resultList;
     }
 
     public void addResult(CheckResult result) {
-        resultList.add(result);
+        resultList.add(0, result);
     }
 
     @Override
